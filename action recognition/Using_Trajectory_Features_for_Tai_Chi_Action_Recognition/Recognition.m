e33bcca27b59e2483fa6753a74bf5b7c8d7324a5
sherlocked0627 <48001722+sherlocked0627@users.noreply.github.com>
2022-06-17 11:32:06 +0800

    Update Recognition.m

--- action recognition/Using_Trajectory_Features_for_Tai_Chi_Action_Recognition/Recognition.m
+++ action recognition/Using_Trajectory_Features_for_Tai_Chi_Action_Recognition/Recognition.m
@@ -1,34 +1,34 @@
 function accuracy = Recognition(traj,label,ratio,k)
-% accuracy：识别准确率
+% accuracy: accuracy of action recognition
 %
-% traj：m*n的cell数组，储存m个点的n个动作轨迹数据（左手手腕）
-% label：1*n动作对应的类别1~k
-% ratio：训练样本和测试样本比例
+% traj锛歴tore trajectory data, size(m*n)
+% label锛歭abels
+% ratio锛歵he ratio of training data and test data
 
-num = 50;  %滤波次数
-leftshoulder = 45;  %左肩点数据在traj中的位置
-rightshoulder = 17;  %右肩点数据在traj中的位置
-lefthand = 1;       %左手点数据在traj中的位置
-n = size(traj,2);     %动作数量
+num = 50;  
+leftshoulder = 45; 
+rightshoulder = 17; 
+lefthand = 1;      
+n = size(traj,2);     % number of actions
 
-vector = cell(1,n);    %储存轨迹向量
+vector = cell(1,n);  
 for i = 1:n
-    A = traj{lefthand,i};        %提取一个动作轨迹
-    A1 = points_filter(A,num);     %轨迹中值滤波
-    A2 = traj_turn(A1,traj,leftshoulder,rightshoulder,i);     %轨迹旋转
-    vector(1,i) = MDL(A2);       %轨迹分割
+    A = traj{lefthand,i};        
+    A1 = points_filter(A,num);  
+    A2 = traj_turn(A1,traj,leftshoulder,rightshoulder,i);
+    vector(1,i) = MDL(A2);
 end
 
 D = cell2mat(vector);   
-Num = 64;      %混合高斯模型聚类数量
-[means, covariances, priors,LL, POSTERIORS] = gmm_para(D,Num);    %计算混合高斯模型参数
+Num = 64;
+[means, covariances, priors,LL, POSTERIORS] = gmm_para(D,Num);
 data = traj(lefthand,:);
 encoding = zeros(Num*6,n);
 for j = 1:n
-encoding(:,j) = vl_fisher(data{1,j}, means, covariances, priors);   %计算Fisher向量
+encoding(:,j) = vl_fisher(data{1,j}, means, covariances, priors);
 end
 
-accuracy = traj_svm(encoding,label,ratio,k);      %利用支持向量机对动作进行分类
+accuracy = traj_svm(encoding,label,ratio,k); 
 end
 
 
