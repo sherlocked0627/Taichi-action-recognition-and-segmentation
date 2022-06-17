32439be506ba5c15d3bc9e8cc8df3d94716cf453
sherlocked_xu <48001722+sherlocked0627@users.noreply.github.com>
2022-06-17 15:22:14 +0800

    Update main.py

--- action segmentation/Tai_Chi_Action_Segmentation/LSTM/main.py
+++ action segmentation/Tai_Chi_Action_Segmentation/LSTM/main.py
@@ -134,7 +134,6 @@ def test():
 
 	print('Test loss:',test_loss/i, 'Test Acc:',test_acc/i)
 	f1(target_seq, predit, [0.1, 0.25, 0.5])
-	np.savetxt('lstm_pre.txt', predit[639:1299])
 
 if __name__ == '__main__':
 	if args.mode == 'train':
